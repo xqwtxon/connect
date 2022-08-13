41fb192c24c7c887aec76f4f51e1ed59f983a866
xqwtxon <xqwtxon@hotmail.com>
2022-08-13 21:48:26 +0800

    New Setup.sh
    This downloads the latest version of 'connect'.sh

---
+++ src/connect/setup.sh
@@ -0,0 +1,30 @@
+version="0.0.1"
+
+
+
+echo -n "[*] Setup Version: "
+sleep 2s
+echo "v$version"
+echo -n "[*] Downloading Connect.sh: "
+curl -sL "https://raw.githubusercontent.com/xqwtxon/connect.sh/v0.0.4/src/connect/main.sh" -o "$TEMP/connect.exec.sh"
+if [ ! "$?" == "0" ]; then
+	echo "error" && echo "[!] Please check your connection and try again!" && exit 1
+else echo "done"
+fi
+echo -n "[*] Downloading Update File: "
+curl -sL "https://raw.githubusercontent.com/xqwtxon/connect.sh/v0.0.4/src/connect/update.sh" -o "connect.update.sh"
+if [ ! "$?" == "0" ]; then
+	echo "error" && echo "[!] Please check your connection and try again!" && exit 1
+else echo "done"
+fi
+echo -n "[*] Move Connect.sh to $PREFIX/bin: "
+sleep 2s
+mv "$TEMP/connect.exec.sh" "$PREFIX/bin/connect" && chmod +x "$PREFIX/bin/connect"
+if [ ! "$?" == "0" ]; then
+	echo "error" && echo "[!] Please check your bash permission! Please make sure the $PREFIX/bin is accessible to you." && exit 1
+else echo "done"
+fi
+echo "[*] Running the Updater Version Check..."
+bash "$PREFIX/tmp/connect.update.sh" --version
+echo "[!] Done! You can now type 'connect --help' to view command information!"
+exit 0
